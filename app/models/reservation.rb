344b5babf93d45d0620d92d55c51ed7cd58a2d53
markbello <mcbello51286@gmail.com>
2018-01-18 16:55:25 -0500

    stuff

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,35 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  # validates :reservation_validator, acceptance: true
+
+  private
+
+
+
+  #validates checkin doesn't overlap with another reservation
+  def conflict?
+    conflict = self.listing.reservations.find do |rez|
+      (rez.checkin < self.checkin) && (rez.checkout > self.checkin)
+    end
+    conflict ? true : false
+  end
+
+  def reservation_validator
+    if self.conflict?
+      errors.add(:checkin, 'Checkin conflict yo')
+    elsif self.checkin < self.checkout
+      errors.add(:checkin, 'Can\'t  check out before u check in gurllll')
+    elsif self.checkin != self.checkout
+      errors.add(:checkin, 'Can\'t checkin and out at the same time u dingus')
+    elsif self.guest_id != self.listing.host_id #validates host and guest are different
+      errors.add(:guest_id, "can\'t host yourself biotch")
+    end
+  end
+
+
+
+
 end
